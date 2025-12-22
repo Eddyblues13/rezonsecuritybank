a8b2093b27b9ff69d12c8571b7a971b0c0e87c66
blueswayne133 <blueswayne133@gmail.com>
2025-12-22 06:32:19 -0800

    enhance search

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html class="no-js" lang="zxx">
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-<head>  
+
+<head>
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <title> rezonsecuritybk.com </title>
@@ -10,7 +11,9 @@
     <link rel="shortcut icon" type="image/x-icon" href="{{asset('dash/favicon.png')}}" />
 
     <!-- Web Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&amp;family=Roboto:wght@300;400;500&amp;display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&amp;family=Roboto:wght@300;400;500&amp;display=swap"
+        rel="stylesheet">
 
 
     <!-- CSS here -->
@@ -23,7 +26,19 @@
     <link rel="stylesheet" href="{{asset('css/glightbox.min.css')}}" />
     <link rel="stylesheet" href="{{asset('css/typography.css')}}" />
     <link rel="stylesheet" href="{{asset('style.css')}}" />
-    
+
+    <!-- Smartsupp Live Chat script -->
+    <script type="text/javascript">
+        var _smartsupp = _smartsupp || {};
+_smartsupp.key = '065ebab24dd535807dde7be2a7df6e6e6a07320c';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+    </script>
+    <noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
 
 
 </head>
@@ -42,57 +57,59 @@
                 <div class="col-lg-12">
                     <div class="nav-inner">
                         <!-- Start Navbar -->
-                    <nav class="navbar navbar-expand-lg">
-                    <a class="navbar-brand style3" href="{{url('/')}}">
-                    <img src="{{asset('dash/logo.png')}}" alt="Logo">
-                        </a>
+                        <nav class="navbar navbar-expand-lg">
+                            <a class="navbar-brand style3" href="{{url('/')}}">
+                                <img src="{{asset('dash/logo.png')}}" alt="Logo">
+                            </a>
 
-<div id="google_translate_element"></div>
+                            <div id="google_translate_element"></div>
 
-<script type="text/javascript">
-function googleTranslateElementInit() {
+                            <script type="text/javascript">
+                                function googleTranslateElementInit() {
   new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
 }
-</script>
+                            </script>
 
-<script type="text/javascript" src="../translate.google.com/translate_a/elementa0d8a0d8.html?cb=googleTranslateElementInit"></script>
+                            <script type="text/javascript"
+                                src="../translate.google.com/translate_a/elementa0d8a0d8.html?cb=googleTranslateElementInit">
+                            </script>
 
 
-                            
-<button class="navbar-toggler mobile-menu-btn" type="button" data-bs-toggle="collapse"
-data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-aria-expanded="false" aria-label="Toggle navigation">
-<span class="toggler-icon"></span>
-   <span class="toggler-icon"></span>
+
+                            <button class="navbar-toggler mobile-menu-btn" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="toggler-icon"></span>
-     </button>
-    <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
-    <ul id="nav" class="navbar-nav ms-auto">
-     <li class="nav-item">
-    <a href="{{url('/')}}" class="active" aria-label="Toggle navigation">Home</a>
-            </li>
-    <li class="nav-item">
-    <a href="{{url('/projects')}}" aria-label="Toggle navigation">Projects</a>
+                                <span class="toggler-icon"></span>
+                                <span class="toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
+                                <ul id="nav" class="navbar-nav ms-auto">
+                                    <li class="nav-item">
+                                        <a href="{{url('/')}}" class="active" aria-label="Toggle navigation">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{url('/projects')}}" aria-label="Toggle navigation">Projects</a>
                                     </li>
                                     </li>
-        <li class="nav-item">
-        <a href="{{url('/services')}}" aria-label="Toggle navigation">Services</a>
+                                    <li class="nav-item">
+                                        <a href="{{url('/services')}}" aria-label="Toggle navigation">Services</a>
                                     </li>
                                     </li>
-        <li class="nav-item">
-        <!--<a href="blog.php" aria-label="Toggle navigation">Blog</a>-->
+                                    <li class="nav-item">
+                                        <!--<a href="blog.php" aria-label="Toggle navigation">Blog</a>-->
                                     </li>
-        <li class="nav-item">
-        <a href="{{url('/about')}}" aria-label="Toggle navigation">About Us</a>
+                                    <li class="nav-item">
+                                        <a href="{{url('/about')}}" aria-label="Toggle navigation">About Us</a>
                                     </li>
 
-        <li class="nav-item">
-        <a href="{{url('/contact')}}" aria-label="Toggle navigation">Contact</a>
+                                    <li class="nav-item">
+                                        <a href="{{url('/contact')}}" aria-label="Toggle navigation">Contact</a>
                                     </li>
                                 </ul>
-        </div> <!-- navbar collapse -->
-        <div class="button add-list-button">
-        <a href="{{route('verify')}}" class="btn">Signup </a>
+                            </div> <!-- navbar collapse -->
+                            <div class="button add-list-button">
+                                <a href="{{route('verify')}}" class="btn">Signup </a>
                             </div>
                         </nav>
                         <!-- End Navbar -->
